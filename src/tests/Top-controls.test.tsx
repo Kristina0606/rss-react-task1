8eafbe71e7d55064666d46fb8aa120fb8ff937ce
Kristina Buben <krisbubenka@icloud.com>
2025-07-21 00:50:25 +0300

    feat: added test to "Displays previously saved search term from localStorage on mount"

--- src/tests/Top-controls.test.tsx
+++ src/tests/Top-controls.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import SearchBar from '../components/Top-controls';
 
 describe('Top-controls component', () => {
@@ -9,7 +9,33 @@ describe('Top-controls component', () => {
       /Enter the Pokémon's name/i
     );
     expect(searchInput).toBeInTheDocument();
+
     const searchButton = screen.getByAltText(/search-icon/i);
     expect(searchButton).toBeInTheDocument();
+  });
+});
+
+describe('SearchBar', () => {
+  it('Displays previously saved search term from localStorage on mount', async () => {
+    localStorage.setItem('pokemon', 'pikachu');
+
+    globalThis.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            id: 25,
+            species: { name: 'pikachu', url: '' },
+            sprites: { front_default: 'pikachu.png' },
+          }),
+      })
+    ) as jest.Mock;
+
+    render(<SearchBar />);
+
+    await waitFor(() => {
+      expect(globalThis.fetch).toHaveBeenCalledWith(
+        'https://pokeapi.co/api/v2/pokemon/pikachu'
+      );
+    });
   });
 });
