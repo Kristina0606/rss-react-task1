6b808ef609d90eeb8df54f5eba2b5fe369f6fa8b
Kristina0606 <krisbubenka@icloud.com>
2025-07-28 14:30:08 +0300

    feat: add ui to notfound page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -2,7 +2,16 @@ import { FC } from 'react';
 import 'react-loading-skeleton/dist/skeleton.css';
 
 const NotFound: FC = () => {
-  return <div>Its Not Found Page!</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen pb-20">
+      <img
+        className="w-100"
+        src="./public/error-404_7921014.png"
+        alt="404-error-img"
+      />
+      <div className="text-xm font-light">This page is not found...</div>
+    </div>
+  );
 };
 
 export default NotFound;
