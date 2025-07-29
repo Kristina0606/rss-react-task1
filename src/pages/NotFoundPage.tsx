ad38c80e29495b27e3656f8cc7b24704c87976fa
Kristina0606 <krisbubenka@icloud.com>
2025-07-29 14:37:05 +0300

    fix

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -6,7 +6,7 @@ const NotFound: FC = () => {
     <div className="flex flex-col items-center justify-center h-screen pb-20">
       <img
         className="w-100"
-        src="./public/error-404_7921014.png"
+        src="./error-404_7921014.png"
         alt="404-error-img"
       />
       <div className="text-xm font-light">This page is not found...</div>
