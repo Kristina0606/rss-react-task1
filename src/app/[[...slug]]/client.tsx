14fa77c62897211f38ac69ebed7529c83f92b3a7
Kristina0606 <krisbubenka@icloud.com>
2025-08-17 23:52:47 +0300

    fix: fix

--- src/app/[[...slug]]/client.tsx
+++ src/app/[[...slug]]/client.tsx
@@ -1,9 +1,14 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import ProviderClient from '../provider';
 
 const App = dynamic(() => import('../../pages/HomePage'), { ssr: false });
 
 export function ClientOnly() {
-  return <App />;
+  return (
+    <ProviderClient>
+      <App />
+    </ProviderClient>
+  );
 }
